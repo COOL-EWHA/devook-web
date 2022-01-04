a86051bb77897a3abc572f444627f2d0d1af2e57
eun-ko <koeun0529@gmail.com>
2022-01-04 13:31:12 +0900

    chore: :lipstick: styled-reset 추가

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
   * {
     box-sizing: border-box;
     font-family: 'Noto Sans KR', sans-serif;
