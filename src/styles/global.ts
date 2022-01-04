edd139aaf0e3a5a7adef6f68d42459c5cf0be01f
eun-ko <koeun0529@gmail.com>
2022-01-04 11:35:11 +0900

    chore: :lipstick: 폰트 추가

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif;
   }
 
   body {
