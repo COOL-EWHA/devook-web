b2b4251097ee5518c55e04a0dc7c7d36614f5705
eun-ko <koeun0529@gmail.com>
2022-01-05 18:06:21 +0900

    feat: :sparkles: NAV_ITEMS 상수로 추가

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,2 +1,17 @@
 export const MOBILE_MAX_WIDTH = '60rem';
 export const DESKTOP_MAX_WIDTH = '1280px';
+
+export const NAV_ITEMS = [
+  {
+    icon: 'bookmarks',
+    label: '북마크 목록',
+  },
+  {
+    icon: 'event_available',
+    label: '읽기 관리',
+  },
+  {
+    icon: 'feed',
+    label: '추천 글 목록',
+  },
+];
