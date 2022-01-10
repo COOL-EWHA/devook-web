19adbe4edb39d9e6e9ce728baa29387eee2a7892
gywlsp <gywls517@gmail.com>
2022-01-10 20:01:02 +0900

    build: :package: recoil 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 import GlobalStyle from 'src/styles/global';
 import MainLayout from 'src/layout/MainLayout';
 import My from 'src/components/my';
+import LoginRedirect from 'src/components/login-redirect';
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <GlobalStyle />
       <MainLayout>
         <Routes>
@@ -22,9 +24,10 @@ function App() {
           >
             <Route path="my" element={<My />} />
           </Route>
+          <Route path="/login-redirect" element={<LoginRedirect />} />
         </Routes>
       </MainLayout>
-    </>
+    </RecoilRoot>
   );
 }
 
