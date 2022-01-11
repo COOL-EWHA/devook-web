9d07c3c2defdc7777438171c692e2cd89de179b7
eun-ko <koeun0529@gmail.com>
2022-01-11 16:02:07 +0900

    refactor: :fire: ModalHeader 파일 제거

--- src/components/base/header/index.ts
+++ src/components/base/header/index.ts
@@ -1,6 +1,5 @@
 import BackHeader from './BackHeader';
 import MainHeader from './MainHeader';
 import GlobalHeader from './GlobalHeader';
-import ModalHeader from './ModalHeader';
 
-export { BackHeader, MainHeader, GlobalHeader, ModalHeader };
+export { BackHeader, MainHeader, GlobalHeader };
