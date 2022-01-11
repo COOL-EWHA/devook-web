ccde9ac6321da1b44f9186573107bbff0dffed96
eun-ko <koeun0529@gmail.com>
2022-01-11 19:37:45 +0900

    chore: :lipstick: App.tsx에 버튼 컴포넌트 테스트용으로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
 
+import MyPage from 'src/pages/my';
+import { Button } from 'src/components/common';
+
 import GlobalStyle from 'src/styles/global';
 import MainLayout from 'src/layout/MainLayout';
-import MyPage from 'src/pages/my';
 
 function App() {
   return (
@@ -16,7 +18,25 @@ function App() {
             element={
               <>
                 <Outlet />
-                <p>hi</p>
+                <Button text="primary 타입" buttonType="primary" />
+                <Button text="primary 타입 (disabled)" buttonType="primary" disabled />
+                <Button text="primary 타입 (GREY 색상 지정)" buttonType="primary" color="GREY" />
+                <Button text="text 타입" />
+                <Button text="text 타입(disabled)" disabled />
+                <Button text="text 타입 (GREY 색상 지정)" color="GREY" />
+                <Button text="line 타입" buttonType="line" />
+                <Button text="line 타입 (height 지정)" buttonType="line" height="5rem" />
+                <Button text="line 타입 (height + isBlock 지정)" buttonType="line" height="5rem" isBlock />
+                <Button text="line 타입 (GREY 색상 지정)" buttonType="line" color="GREY" />
+                <Button text="line 타입 (disabled)" buttonType="line" disabled />
+                <Button
+                  text="href + line 타입"
+                  buttonType="line"
+                  href="https://yamoo9.gitbook.io/typescript/interface/extends"
+                />
+                <Button text="아이콘 있을때" iconType="person" />
+                <Button text="아이콘 있을때 (disabled)" iconType="person" disabled />
+                <Button text="아이콘 있을때(GREY 색상 지정)" iconType="person" color="GREY" />
               </>
             }
           >
