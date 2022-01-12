9179782df82514ce61f1f930154971d1a37ca710
gywlsp <gywls517@gmail.com>
2022-01-12 16:35:17 +0900

    chore: :pencil2: Github OAuth data 추가

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,6 @@
+import GithubIcon from 'src/components/assets/icons/github';
 import GoogleIcon from 'src/components/assets/icons/google';
+import { getQueryString } from 'src/lib/utils';
 
 export const MOBILE_MAX_WIDTH = '60rem';
 export const DESKTOP_MAX_WIDTH = '1280px';
@@ -23,13 +25,21 @@ export const SUB_ROUTES = [{ pathname: '/my', title: '마이페이지' }];
 export const OAUTH_DATA = {
   google: {
     Icon: GoogleIcon,
-    params: {
+    link: `https://accounts.google.com/o/oauth2/v2/auth${getQueryString({
       include_granted_scopes: true,
       state: 'state_parameter_passthrough_value',
       scope: 'email+profile',
       response_type: 'code',
       redirect_uri: process.env.REACT_APP_OAUTH_REDIRECT_URI,
       client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-    },
+    })}`,
+  },
+  github: {
+    Icon: GithubIcon,
+    link: `https://github.com/login/oauth/authorize${getQueryString({
+      scope: 'id,name,email,avatar_url',
+      redirect_uri: process.env.REACT_APP_OAUTH_REDIRECT_URI,
+      client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
+    })}`,
   },
 };
