00245a46d082ab3622b4b77cb6cac70ab6ffa3e5
eun-ko <koeun0529@gmail.com>
2022-01-12 14:52:31 +0900

    feat: :sparkles: 컴포넌트 import 파일 추가

--- src/components/my/index.ts
+++ src/components/my/index.ts
@@ -1,5 +1,7 @@
 import LoginButton from './LoginButton';
 import LoginButtons from './LoginButtons';
-import UserInfoCard from './UserInfoCard';
+import LogoutButton from './LogoutButton';
+import WithdrawButton from './WithdrawButton';
+import UserProfileCard from './UserProfileCard';
 
-export { LoginButton, LoginButtons, UserInfoCard };
+export { LoginButton, LoginButtons, LogoutButton, WithdrawButton, UserProfileCard };
