eab366494ab6f279f246c3b6fbf3aca9e569c106
gywlsp <gywls517@gmail.com>
2022-01-12 12:40:52 +0900

    feat: :sparkles: react-query Provider 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 import GlobalStyle from 'src/styles/global';
 import App from './App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <RecoilRoot>
-        <GlobalStyle />
-        <App />
-      </RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <GlobalStyle />
+          <App />
+        </RecoilRoot>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
