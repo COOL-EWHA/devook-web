0b4aea4bb0a2a639e733bdb6124cc6880cbd0a13
eun-ko <koeun0529@gmail.com>
2022-01-14 23:57:57 +0900

    feat: :sparkles: BookmarkListPage에 태그관련 컴포넌트 적용

--- src/pages/BookmarkListPage.tsx
+++ src/pages/BookmarkListPage.tsx
@@ -1,28 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
-import { BookmarkSearchInput } from 'src/components/bookmarks';
-import { PostCard, ScrollToTopButton } from 'src/components/common';
+import { BookmarkSearchInput, BookmarkTagList } from 'src/components/bookmarks';
+import { PostCard, FixedButton, Modal } from 'src/components/common';
 
-import { BOOKMARK_LIST } from 'src/constant/mockData';
+import { BOOKMARK_LIST, TAG_LIST } from 'src/constant/mockData';
 
 function BookmarkListPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleScrollTopButtonClick = () => {
+    window.scroll({
+      behavior: 'smooth',
+      top: 0,
+    });
+  };
+
+  const handleModalOpenState = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth > 1024) {
+      setIsModalOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <>
+    <Wrapper>
       <Outlet />
-      <BookmarkSearchInput />
-      {BOOKMARK_LIST.map((bookmark) => (
-        <PostCard
-          key={bookmark.title}
-          title={bookmark.title}
-          thumbnail={bookmark.thumbnail}
-          description={bookmark.description}
-          tags={bookmark.tags}
-        />
-      ))}
-      <ScrollToTopButton />
-    </>
+      <BookmarksWrapper>
+        <BookmarkSearchInput />
+        {BOOKMARK_LIST.map((bookmark) => (
+          <PostCard
+            key={bookmark.title}
+            title={bookmark.title}
+            thumbnail={bookmark.thumbnail}
+            description={bookmark.description}
+            tags={bookmark.tags}
+          />
+        ))}
+      </BookmarksWrapper>
+      <FixedButton buttonType="tag" iconType="tag" onClick={handleModalOpenState} />
+      <FixedButton buttonType="scroll" iconType="expand_less" onClick={handleScrollTopButtonClick} />
+      {!isModalOpen && <BookmarkTagList tags={TAG_LIST} isModalOpen={isModalOpen} />}
+      {isModalOpen && (
+        <Modal setIsModalOpen={setIsModalOpen} onComplete={handleModalOpenState} title="태그 선택">
+          <BookmarkTagList tags={TAG_LIST} isModalOpen={isModalOpen} />
+        </Modal>
+      )}
+    </Wrapper>
   );
 }
 
 export default BookmarkListPage;
+
+const BookmarksWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+`;
