735acd1159ca7071e2801afffddc31efdc916144
eun-ko <koeun0529@gmail.com>
2022-01-17 17:03:51 +0900

    refactor: :recycle: isModalOpen 관련 로직을 TagListButton 고정 컴포넌트로 이동

--- src/components/common/TagListButton.tsx
+++ src/components/common/TagListButton.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { FixedButton } from 'src/components/common';
+import { FixedButton, Modal, TagList } from 'src/components/common';
+import { TAG_LIST } from 'src/constant/mockData';
 
-interface ITagListButtonProps {
-  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}
+function TagListButton() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-function TagListButton({ setIsModalOpen }: ITagListButtonProps) {
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleTagSubmit = () => {
+    // @TO_BE_IMPROVED: fetch tag filtering api
+  };
+
+  const handleResize = () => {
+    // @TO_BE_IMPROVED: debounce 적용
+    if (window.innerWidth > 1024) {
+      setIsModalOpen(false);
+    }
+  };
+
   const handleClick = () => {
     setIsModalOpen(true);
   };
 
-  return <FixedButton type="tag" iconType="tag" onClick={handleClick} />;
+  return (
+    <>
+      <FixedButton type="tag" iconType="tag" onClick={handleClick} />
+      {!isModalOpen && <TagList tags={TAG_LIST} isModalOpen={isModalOpen} />}
+      {isModalOpen && (
+        <Modal setIsModalOpen={setIsModalOpen} onComplete={handleTagSubmit} title="태그 선택">
+          <TagList tags={TAG_LIST} isModalOpen={isModalOpen} />
+        </Modal>
+      )}
+    </>
+  );
 }
 
 export default TagListButton;
--- src/pages/BookmarkListPage.tsx
+++ src/pages/BookmarkListPage.tsx
@@ -1,44 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { BookmarkSearchInput, BookmarkTagList } from 'src/components/bookmarks';
-import { PostCard, FixedButton, Modal } from 'src/components/common';
+import { BookmarkSearchInput } from 'src/components/bookmarks';
+import { PostCard, TagListButton, ScrollToTopButton } from 'src/components/common';
 
-import { BOOKMARK_LIST, TAG_LIST } from 'src/constant/mockData';
+import { BOOKMARK_LIST } from 'src/constant/mockData';
 
 function BookmarkListPage() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleScrollTopButtonClick = () => {
-    window.scroll({
-      behavior: 'smooth',
-      top: 0,
-    });
-  };
-
-  const handleFixedTagButtonClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleTagSubmit = () => {
-    //
-  };
-
-  const handleResize = () => {
-    // @TO_BE_IMPROVED: debounce 적용
-    if (window.innerWidth > 1024) {
-      setIsModalOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
     <Wrapper>
       <Outlet />
@@ -54,14 +23,8 @@ function BookmarkListPage() {
           />
         ))}
       </BookmarksWrapper>
-      {!isModalOpen && <BookmarkTagList tags={TAG_LIST} isModalOpen={isModalOpen} />}
-      {isModalOpen && (
-        <Modal setIsModalOpen={setIsModalOpen} onComplete={handleTagSubmit} title="태그 선택">
-          <BookmarkTagList tags={TAG_LIST} isModalOpen={isModalOpen} />
-        </Modal>
-      )}
-      <FixedButton buttonType="tag" iconType="tag" onClick={handleFixedTagButtonClick} />
-      <FixedButton buttonType="scroll" iconType="expand_less" onClick={handleScrollTopButtonClick} />
+      <TagListButton />
+      <ScrollToTopButton />
     </Wrapper>
   );
 }
