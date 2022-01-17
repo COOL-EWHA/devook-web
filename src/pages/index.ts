c65207a8b74b1b28db97ecff9927915bed5442bd
gywlsp <gywls517@gmail.com>
2022-01-17 21:53:24 +0900

    refactor: :recycle: 테스트 도메인일 때 테스트 로그인 버튼 표시

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
 import BookmarkListPage from './BookmarkListPage';
 import MyPage from './MyPage';
 import OauthRedirectPage from './OauthRedirectPage';
-import TestLoginPage from './TestLoginPage';
 
-export { BookmarkListPage, MyPage, OauthRedirectPage, TestLoginPage };
+export { BookmarkListPage, MyPage, OauthRedirectPage };
