381ed2d2782a74d6e3b9245912fe596a330b2446
eun-ko <koeun0529@gmail.com>
2022-01-18 22:39:13 +0900

    feat: :sparkles: 북마크 목록 조회, 북마크 삭제 관련 axios 코드 추가

--- src/lib/api/bookmarks.ts
+++ src/lib/api/bookmarks.ts
@@ -1,4 +1,19 @@
 import { apiClient } from '.';
 
-export const createBookmark = (params: { id?: number; url: string; memo?: string }): Promise<void> =>
+import { BookmarkPostPreview, CreateBookmarkParams } from 'src/types';
+
+export const createBookmark = (params: CreateBookmarkParams): Promise<void> =>
   apiClient.post(`/bookmarks`, params).then((res) => res.data);
+
+export const deleteBookmark = ({ id }: { id?: number }) => apiClient.delete(`/bookmarks/${id}`).then((res) => res.data);
+
+export const getBookmarkList = ({
+  tags,
+  cursor,
+  limit,
+}: {
+  tags?: string;
+  cursor: number;
+  limit: number;
+}): Promise<BookmarkPostPreview[]> =>
+  apiClient.get(`/bookmarks?tags=${tags}&cursor=${cursor}&limit=${limit}`).then((res) => res.data);
--- src/types/bookmarks.ts
+++ src/types/bookmarks.ts
@@ -1,3 +1,4 @@
 import { IBookmarkPost } from 'src/interfaces';
 
 export type BookmarkPostPreview = Pick<IBookmarkPost, 'id' | 'title' | 'thumbnail' | 'description' | 'url' | 'tags'>;
+export type CreateBookmarkParams = Pick<IBookmarkPost, 'url' | 'memo'>;
