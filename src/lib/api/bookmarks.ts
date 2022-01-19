cf630b23282b958b288e9c6ec59e0e1785200654
gywlsp <gywls517@gmail.com>
2022-01-19 19:56:29 +0900

    chore: :fire: BookmarkDeleteParams 타입 삭제

--- src/lib/api/bookmarks.ts
+++ src/lib/api/bookmarks.ts
@@ -1,12 +1,12 @@
 import { apiClient } from '.';
 
-import { BookmarkPreview, BookmarkCreateParams, BookmarkDeleteParams, BookmarkListParams } from 'src/types';
+import { BookmarkPreview, BookmarkCreateParams, BookmarkListParams } from 'src/types';
 import { getQueryString } from 'src/lib/utils';
 
 export const createBookmark = (params: BookmarkCreateParams): Promise<void> =>
   apiClient.post(`/bookmarks`, params).then((res) => res.data);
 
-export const deleteBookmark = ({ id }: BookmarkDeleteParams): Promise<void> =>
+export const deleteBookmark = (id: number): Promise<void> =>
   apiClient.delete(`/bookmarks/${id}`).then((res) => res.data);
 
 export const getBookmarkList = (params: BookmarkListParams): Promise<BookmarkPreview[]> =>
--- src/types/bookmarks.ts
+++ src/types/bookmarks.ts
@@ -3,6 +3,5 @@ import { ListRequestParams } from '.';
 
 export type BookmarkPreview = Pick<IBookmark, 'id' | 'title' | 'thumbnail' | 'description' | 'url' | 'tags'>;
 export type BookmarkCreateParams = Pick<IBookmark, 'url' | 'memo'>;
-export type BookmarkDeleteParams = Pick<IBookmark, 'id'>;
 export type BookmarkListParams = ListRequestParams & Pick<IBookmark, 'tags'> & { q?: string };
 export type BookmarkListFilter = Pick<BookmarkListParams, 'tags' | 'q'>;
