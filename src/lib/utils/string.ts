35c7548ee9e6b6597e20d633be92ccd46e564cc8
gywlsp <gywls517@gmail.com>
2022-01-19 11:15:32 +0900

    feat: :sparkles: getQueryString 함수에서 params 중 null이나 undefined 값 포함 안 되게 변경

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -1,5 +1,7 @@
-export const getQueryString = (queryParameter: Record<string, any> = {}) =>
-  Object.entries(queryParameter).reduce((acc, curr, i) => {
+export const getQueryString = (params: Record<string, any> = {}) => {
+  const filteredParams = Object.entries(params).filter(([_, value]) => value !== null && value !== undefined);
+  return filteredParams.reduce((acc, curr, i) => {
     const [key, value] = curr;
     return acc.concat(`${i ? '&' : '?'}${key}=${value}`);
   }, '');
+};
