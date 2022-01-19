05147d260c3eafe75ad0dfde1749c2b11f645ce3
gywlsp <gywls517@gmail.com>
2022-01-19 19:50:22 +0900

    feat: :sparkles: getQueryString 기능 추가
    빈 문자열, 빈 배열 제외

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -1,7 +1,10 @@
 export const getQueryString = (params: Record<string, any> = {}) => {
-  const filteredParams = Object.entries(params).filter(([_, value]) => value !== null && value !== undefined);
+  const filteredParams = Object.entries(params).filter(
+    ([_, value]) => value !== null && value !== undefined && value !== '' && (!Array.isArray(value) || value.length),
+  );
   return filteredParams.reduce((acc, curr, i) => {
     const [key, value] = curr;
-    return acc.concat(`${i ? '&' : '?'}${key}=${value}`);
+    const refinedValue = Array.isArray(value) ? value.join(',') : value;
+    return acc.concat(`${i ? '&' : '?'}${key}=${refinedValue}`);
   }, '');
 };
