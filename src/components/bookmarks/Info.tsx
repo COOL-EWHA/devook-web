32b293fe4912323a43c26c3c31221a1df2660658
eun-ko <koeun0529@gmail.com>
2022-01-20 21:25:45 +0900

    refactor: :recycle: Section 컴포넌트의 isEditable속성을 rightComponent로 교체

--- src/components/bookmarks/Info.tsx
+++ src/components/bookmarks/Info.tsx
@@ -1,22 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import { LabeledText, Section } from 'src/components/common';
-import { useBookmarkDetail } from 'src/lib/hooks';
+import { LabeledText, Section, Button, Modal, Input, Textarea } from 'src/components/common';
+import { useBookmark, useBookmarkMemoEdit } from 'src/lib/hooks';
 
 function BookmarkInfo() {
-  const { data, isLoading } = useBookmarkDetail();
+  const { data, isLoading } = useBookmark();
+  const { isModalOpen, openModal, onSubmit, onChange, memo, setIsModalOpen } = useBookmarkMemoEdit({
+    originalMemo: data?.memo,
+  });
 
   return (
     <>
       {isLoading && <div>loading...</div>}
       {data && (
-        <Section title="북마크 정보" isEditable type="secondary">
-          <LabeledText label="생성 날짜" value={data?.createdAt.toString()} />
-          <LabeledText label="메모" value={data?.memo} />
+        <Section
+          title="북마크 정보"
+          rightComponent={<Button buttonType="text" text="수정" color="GREY" onClick={openModal} />}
+          type="secondary"
+        >
+          <LabeledText label="생성 날짜" value={data.createdAt.toString()} />
+          <LabeledText label="메모" value={data.memo} />
         </Section>
+      )}
+      {isModalOpen && (
+        <Modal setIsModalOpen={setIsModalOpen} onComplete={onSubmit} title="메모 수정하기">
+          <InputWrapper>
+            <Input name="url" value={data?.createdAt.toString()} label="생성 날짜" disabled />
+            <Textarea
+              name="memo"
+              value={memo}
+              onChange={onChange}
+              label="메모"
+              placeholder="이 북마크와 관련된 메모를 수정해보세요"
+            />
+          </InputWrapper>
+        </Modal>
       )}
     </>
   );
 }
 
 export default BookmarkInfo;
+
+const InputWrapper = styled.div`
+  padding: 2rem;
+`;
--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -1,49 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { P, Button, Modal, Input, Textarea } from 'src/components/common';
-import { useBookmarkDetail, useBookmarkMemoEdit } from 'src/lib/hooks';
+import { P } from 'src/components/common';
 import { GREY } from 'src/constant';
 
 interface ISectionProps {
   type?: 'primary' | 'secondary';
   title: string;
-  isEditable?: boolean;
+  rightComponent?: React.ReactNode;
   children?: React.ReactNode;
 }
 
-function Section({ type = 'primary', title, isEditable = false, children }: ISectionProps) {
-  const { data } = useBookmarkDetail();
-  const { isModalOpen, openModal, onSubmit, onChange, memo, setIsModalOpen } = useBookmarkMemoEdit({
-    originalMemo: data?.memo,
-  });
-
+function Section({ type = 'primary', title, rightComponent, children }: ISectionProps) {
   return (
-    <>
-      <Wrapper>
-        <Header>
-          <P fontSize={type === 'primary' ? '2.2rem' : '1.8rem'} fontWeight={500}>
-            {title}
-          </P>
-          {isEditable && <Button buttonType="text" text="수정" color="GREY" onClick={openModal} />}
-        </Header>
-        {children}
-      </Wrapper>
-      {isModalOpen && (
-        <Modal setIsModalOpen={setIsModalOpen} onComplete={onSubmit} title="메모 수정하기">
-          <InputWrapper>
-            <Input name="url" value={data?.createdAt.toString()} label="생성 날짜" disabled />
-            <Textarea
-              name="memo"
-              value={memo}
-              onChange={onChange}
-              label="메모"
-              placeholder="이 북마크와 관련된 메모를 수정해보세요"
-            />
-          </InputWrapper>
-        </Modal>
-      )}
-    </>
+    <Wrapper>
+      <Header>
+        <P fontSize={type === 'primary' ? '2.2rem' : '1.8rem'} fontWeight={500}>
+          {title}
+        </P>
+        {rightComponent}
+      </Header>
+      {children}
+    </Wrapper>
   );
 }
 
@@ -61,8 +39,4 @@ const Header = styled.div`
   padding-bottom: 1rem;
   border-bottom: 1px solid ${GREY[300]};
   margin-bottom: 1.2rem;
-`;
-
-const InputWrapper = styled.div`
-  padding: 2rem;
 `;
