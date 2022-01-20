38717cb4f34bad0646bfce2fe5c32b80c91041b6
gywlsp <gywls517@gmail.com>
2022-01-20 18:32:39 +0900

    chore: :pencil2: SUB_ROUTES 데이터 수정
    마이페이지 데이터 삭제, pathname /bookmarks일 때 title 추가

--- src/constant/route.ts
+++ src/constant/route.ts
@@ -13,7 +13,4 @@ export const NAV_ITEMS = [
   },
 ];
 
-export const SUB_ROUTES = [
-  { pathname: '/my', title: '마이페이지' },
-  { pathname: '/bookmarks', title: '' },
-];
+export const SUB_ROUTES = [{ pathname: '/bookmarks', title: '북마크 상세' }];
