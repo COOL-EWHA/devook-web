bc8f8fd95145868b406920593215d3d7d8ce4c35
eun-ko <koeun0529@gmail.com>
2022-01-21 14:33:53 +0900

    feat: :sparkles: GNB active 상태 식별 추가

--- src/constant/route.ts
+++ src/constant/route.ts
@@ -2,14 +2,17 @@ export const NAV_ITEMS = [
   {
     iconType: 'bookmarks',
     label: '북마크 목록',
+    to: '/',
   },
   {
     iconType: 'event_available',
     label: '읽기 관리',
+    to: '/reading-management',
   },
   {
     iconType: 'feed',
     label: '추천 글 목록',
+    to: '/recommendations',
   },
 ];
 
