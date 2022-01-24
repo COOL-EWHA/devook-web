9f2bba26779d737b020387d2f0f87b3edbced12b
eun-ko <koeun0529@gmail.com>
2022-01-24 10:33:44 +0900

    feat: :sparkles: 검색 기능 추가

--- src/components/bookmarks/SearchInput.tsx
+++ src/components/bookmarks/SearchInput.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import debounce from 'lodash/debounce';
+import { useRecoilState } from 'recoil';
 
 import { Input } from 'src/components/common';
+import { bookmarkListFilter } from 'src/lib/store/bookmarks';
 
 function BookmarkSearchInput() {
-  return <Input iconType="search" placeholder="북마크를 검색해보세요." />;
+  const [filter, setFilter] = useRecoilState(bookmarkListFilter);
+  const [keyword, setKeyword] = useState('');
+
+  const debounceSearch = useCallback(
+    debounce((keyword) => {
+      setFilter({ ...filter, q: keyword });
+    }, 500),
+    [],
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setKeyword(value);
+    debounceSearch(value);
+  };
+
+  return <Input iconType="search" placeholder="북마크를 검색해보세요." value={keyword} onChange={handleChange} />;
 }
 
 export default BookmarkSearchInput;
