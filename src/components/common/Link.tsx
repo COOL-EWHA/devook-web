a4a58716a9e2ff9904de821d53c944e9bb8bb4ea
gywlsp <gywls517@gmail.com>
2022-01-25 18:59:49 +0900

    feat: :sparkles: common Link에 disabled props 추가

--- src/components/common/Link.tsx
+++ src/components/common/Link.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link as RouterLink, LinkProps } from 'react-router-dom';
 
-type ILinkProps = LinkProps & React.RefAttributes<HTMLAnchorElement>;
+type ILinkProps = LinkProps & React.RefAttributes<HTMLAnchorElement> & { disabled?: boolean };
 
-function Link({ to, children, className }: ILinkProps) {
+function Link({ className, to, disabled = false, children }: ILinkProps) {
+  const cn = disabled ? `${className} disabled-link` : className;
+
   return (
-    <StyledLink className={className} to={to}>
+    <StyledLink className={cn} to={to}>
       {children}
     </StyledLink>
   );
@@ -17,4 +19,7 @@ export default Link;
 const StyledLink = styled(RouterLink)`
   text-decoration: none;
   width: 100%;
+  .disabled-link {
+    pointer-events: none;
+  }
 `;
