f41ccaaf0c76ba3a3dcf5cc6df7b91ff66880f7e
gywlsp <gywls517@gmail.com>
2022-01-25 14:21:02 +0900

    chore: :label: RelatedPostListParams 타입 추가

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -3,6 +3,7 @@ import { ListRequestParams } from '.';
 
 export type PostPreview = Pick<IPost, 'id' | 'title' | 'thumbnail' | 'description' | 'url' | 'tags'>;
 export type PostListParams = ListRequestParams & Pick<IPost, 'tags'> & { q?: string };
+export type RelatedPostListParams = BookmarkRelatedPostListParams | PostRelatedPostListParams;
 export type BookmarkRelatedPostListParams = ListRequestParams & { bookmarkId: number };
 export type PostRelatedPostListParams = ListRequestParams & { postId: number };
 export type PostListFilter = Pick<PostListParams, 'tags' | 'q'>;
