7ff21d12a60b3d73328db511f586101497c92d8d
gywlsp <gywls517@gmail.com>
2022-01-26 17:01:09 +0900

    chore: :fire: getPost api 삭제

--- src/lib/api/posts.ts
+++ src/lib/api/posts.ts
@@ -1,9 +1,6 @@
 import { apiClient } from '.';
 import { PostPreview, RelatedPostListParams } from 'src/types';
 import { getQueryString } from 'src/lib/utils';
-import { IPost } from 'src/interfaces';
 
 export const getRelatedPostList = (params: RelatedPostListParams): Promise<PostPreview[]> =>
   apiClient.get(`/posts${getQueryString(params)}`).then((res) => res.data);
-
-export const getPost = (id: number): Promise<IPost> => apiClient.get(`/posts/${id}`).then((res) => res.data);
