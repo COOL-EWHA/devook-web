9b3c0b0ad6e0fc75eb2519a725cffbc3136ef762
eun-ko <koeun0529@gmail.com>
2022-01-26 22:15:52 +0900

    fix: :bug: tsc 테스트 통과하도록 수정

--- src/components/posts/List.tsx
+++ src/components/posts/List.tsx
@@ -3,7 +3,7 @@ import { InfiniteData } from 'react-query';
 import styled from 'styled-components';
 
 import { PostListSkeleton } from '.';
-import { PostPreviewCard } from 'src/components/common';
+import { PostPreviewCard } from 'src/components/posts';
 import { PostPreview } from 'src/types';
 import { BOOKMARK_FETCH_LIMIT } from 'src/constant';
 
@@ -20,16 +20,17 @@ function PostList({ isLoading, data, listEndRef, fetchLimit = BOOKMARK_FETCH_LIM
       {isLoading && <PostListSkeleton fetchLimit={fetchLimit} />}
       {data?.pages.map((posts) =>
         posts?.map((post: PostPreview) => {
-          const { id, title, thumbnail, description, tags } = post;
+          const { id, title, thumbnail, description, tags, isBookmarked, url } = post;
           return (
             <PostPreviewCard
               key={id}
-              id={id}
+              bookmarkId={id}
               title={title}
               thumbnail={thumbnail}
               description={description}
               tags={tags}
-              isBookmarked={false}
+              isBookmarked={isBookmarked}
+              url={url}
             />
           );
         }),
