e3217a4e5d603caf012eb45faace32aee11ba627
gywlsp <gywls517@gmail.com>
2022-01-26 16:48:32 +0900

    refactor: :recycle: useRelatedPostList 리팩토링

--- src/lib/hooks/posts.ts
+++ src/lib/hooks/posts.ts
@@ -1,21 +1,18 @@
 import { useQuery } from 'react-query';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
-import { RELATED_POST_FETCH_LIMIT } from 'src/constant';
+import { NO_REFETCH, RELATED_POST_FETCH_LIMIT } from 'src/constant';
 import { getPost, getRelatedPostList } from 'src/lib/api';
 import { postKeys } from 'src/lib/utils/queryKeys';
 
-export const useRelatedPostList = () => {
-  const { pathname } = useLocation();
-  const { id = '' } = useParams();
-
+export const useRelatedPostList = (bookmarkId: number) => {
   const filter = {
-    ...(pathname.includes('bookmarks') ? { bookmarkId: Number(id) } : { postId: Number(id) }),
+    bookmarkId,
     limit: RELATED_POST_FETCH_LIMIT,
   };
 
   const queryFn = () => getRelatedPostList(filter);
-  const { data, isLoading } = useQuery(postKeys.list(filter), queryFn);
+  const { data, isLoading } = useQuery(postKeys.list(filter), queryFn, NO_REFETCH);
 
   return { data, isLoading };
 };
@@ -25,7 +22,7 @@ export const usePost = () => {
   const postId = Number(params.id ?? '');
 
   const queryFn = () => getPost(postId);
-  const { data, isLoading } = useQuery(postKeys.detail(postId), queryFn);
+  const { data, isLoading } = useQuery(postKeys.detail(postId), queryFn, NO_REFETCH);
 
   return { id: postId, data, isLoading };
 };
