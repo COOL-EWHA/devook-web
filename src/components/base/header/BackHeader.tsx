8b2a744d622604000beeaee80c897949c2b73a7b
gywlsp <gywls517@gmail.com>
2022-02-03 12:23:51 +0900

    refactor: :recycle: BackHeader 스타일 코드 리팩토링

--- src/components/base/header/BackHeader.tsx
+++ src/components/base/header/BackHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
 import { SidebarToggleButton } from 'src/components/my';
@@ -41,41 +41,28 @@ export default function BackHeader({ onBack, onComplete, title }: IBackHeaderPro
   );
 }
 
-const getWrapperStyle = (isModalHeader?: boolean) => {
-  if (isModalHeader) {
-    return css`
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
+const Wrapper = styled.div<{ isModalHeader?: boolean }>`\
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${WHITE};
+  ${({ isModalHeader }) =>
+    isModalHeader
+      ? `
       padding: 1.6rem 2rem;
-    `;
-  }
-  return css`
-    position: fixed;
-    top: 0;
-    z-index: 12;
-
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    width: 100%;
-    padding: 0 2rem;
-    background-color: ${WHITE};
-
-    @media screen and (min-width: 1025px) {
-      display: none;
-    }
-
-    @media screen and (max-width: 1024px) {
+    `
+      : `
+      @media screen and (min-width: 1025px) {
+        display: none;
+      }
+      position: fixed;
+      top: 0;
+      z-index: 12;
+      width: 100%;
+      padding: 0 2rem;
       height: 5.2rem;
       border-bottom: 1px solid ${GREY[300]};
-    }
-  `;
-};
-
-const Wrapper = styled.div<{ isModalHeader?: boolean }>`
-  ${({ isModalHeader }) => getWrapperStyle(isModalHeader)};
+  `};
 `;
 
 const Title = styled(P)<{ isModalHeader?: boolean }>`
