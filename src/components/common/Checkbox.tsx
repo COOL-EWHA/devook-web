396910d3b34753c18eb0469d9616bed885b209b1
gywlsp <gywls517@gmail.com>
2022-02-03 23:17:00 +0900

    refactor: :recycle: Checkbox 리팩토링

--- src/components/common/Checkbox.tsx
+++ src/components/common/Checkbox.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import styled from 'styled-components';
 
 import { MaterialIcon } from '.';
 import { CACTUS_GREEN, GREY } from 'src/constant';
@@ -9,14 +11,19 @@ interface ICheckboxProps {
 }
 
 function Checkbox({ isChecked, onClick }: ICheckboxProps) {
+  const [color, hoverColor] = isChecked ? [CACTUS_GREEN[500], CACTUS_GREEN[700]] : [GREY[300], GREY[500]];
   return (
     <>
-      {isChecked && (
-        <MaterialIcon type="check_box" color={CACTUS_GREEN[500]} hoverColor={CACTUS_GREEN[600]} onClick={onClick} />
-      )}
-      {!isChecked && <MaterialIcon type="check_box" color={GREY[400]} hoverColor={GREY[500]} onClick={onClick} />}
+      <Input id="checkbox" type="checkbox" checked={isChecked} onChange={onClick} />
+      <label htmlFor="checkbox">
+        <MaterialIcon type="check_box" color={color} hoverColor={hoverColor} />
+      </label>
     </>
   );
 }
 
 export default Checkbox;
+
+const Input = styled.input`
+  display: none;
+`;
