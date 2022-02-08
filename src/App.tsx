f07148a47b884f943a90f08db76d714ae6504f7a
eun-ko <koeun0529@gmail.com>
2022-02-08 20:32:25 +0900

    refactor: :zap: route 기반으로 코드 스플리팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import MainLayout from 'src/layout/MainLayout';
-import { BookmarkDetailPage, BookmarkListPage, OauthRedirectPage, FurtherReadPage, ToReadPage } from 'src/pages';
 import { useAuthRefresh } from 'src/lib/hooks';
+
+const FurtherReadPage = lazy(() => import('./pages/FurtherReadPage'));
+const OauthRedirectPage = lazy(() => import('./pages/OauthRedirectPage'));
+const BookmarkListPage = lazy(() => import('./pages/BookmarkListPage'));
+const BookmarkDetailPage = lazy(() => import('./pages/BookmarkDetailPage'));
+const ToReadPage = lazy(() => import('./pages/ToReadPage'));
 
 function App() {
   useAuthRefresh();
 
   return (
     <MainLayout>
-      <Routes>
-        <Route path="/" element={<FurtherReadPage />} />
-        <Route path="/oauth-redirect" element={<OauthRedirectPage />} />
-        <Route path="/bookmarks" element={<BookmarkListPage />} />
-        <Route path="/bookmarks/:id" element={<BookmarkDetailPage />} />
-        <Route path="/to-read" element={<ToReadPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<FurtherReadPage />} />
+          <Route path="/oauth-redirect" element={<OauthRedirectPage />} />
+          <Route path="/bookmarks" element={<BookmarkListPage />} />
+          <Route path="/bookmarks/:id" element={<BookmarkDetailPage />} />
+          <Route path="/to-read" element={<ToReadPage />} />
+        </Routes>
+      </Suspense>
     </MainLayout>
   );
 }
