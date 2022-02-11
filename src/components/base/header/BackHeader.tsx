a78b5ba348dbe540371bcc465c50d9f37f643fb3
eun-ko <koeun0529@gmail.com>
2022-02-11 11:40:47 +0900

    refactor: :recycle: backHeader가 sideBar에 있을때를 위한 UI 및 로직 추가

--- src/components/base/header/BackHeader.tsx
+++ src/components/base/header/BackHeader.tsx
@@ -2,42 +2,61 @@ import React from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
-import { SidebarToggleButton } from 'src/components/my';
 import { IconButton, P } from 'src/components/common';
 import { GREY, WHITE } from 'src/constant';
 
 interface IBackHeaderProps {
   title: string;
+  isForSidebar?: boolean;
+  onClick?: () => void;
 }
 
-export default function BackHeader({ title }: IBackHeaderProps) {
+export default function BackHeader({ title, isForSidebar = false, onClick }: IBackHeaderProps) {
   const navigate = useNavigate();
 
   const handleBack = () => {
+    if (onClick) {
+      onClick();
+      return;
+    }
     navigate(-1);
   };
 
   return (
-    <Wrapper>
+    <Wrapper isForSidebar={isForSidebar}>
       <IconButton iconType="arrow_back_ios" onClick={handleBack} />
       <Title>{title}</Title>
-      <SidebarToggleButton />
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ isForSidebar?: boolean }>`
   @media screen and (min-width: 1025px) {
-    display: none;
+    ${({ isForSidebar }) =>
+      isForSidebar
+        ? `
+        position: fixed;
+        top: 0;
+        right:0;
+        padding: 0 2rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width:320px; 
+        height: 5.2rem;
+        border-bottom: 1px solid ${GREY[300]};
+        `
+        : `display: none;`}
   }
   @media screen and (max-width: 1024px) {
     position: fixed;
     top: 0;
+    ${({ isForSidebar }) => isForSidebar && 'right:0'};
     z-index: 12;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 100%;
+    width: ${({ isForSidebar }) => (isForSidebar ? '320px' : '100%')};
     height: 5.2rem;
     padding: 0 2rem;
     border-bottom: 1px solid ${GREY[300]};
