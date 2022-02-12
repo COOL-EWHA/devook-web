9d608a8a7b143140f8472e8c7c8a96883bf51a2c
eun-ko <koeun0529@gmail.com>
2022-02-12 11:22:56 +0900

    refactor: :recycle: useNotificationCard에서 라우팅과 passedDate을 반환하는것을 제거하고 useNotificationEdit으로 수정

--- src/lib/hooks/notifications.ts
+++ src/lib/hooks/notifications.ts
@@ -1,13 +1,12 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { useInfiniteQuery } from 'react-query';
-import { useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 
 import { getNotificationList, editNotification } from 'src/lib/api';
 import { notificationKeys } from 'src/lib/utils/queryKeys';
 import { isMySidebarOpen } from 'src/lib/store';
-import { NOTIFICATION_ROUTES, POST_LIST_FETCH_LIMIT } from 'src/constant';
+import { POST_LIST_FETCH_LIMIT } from 'src/constant';
 import { INotification } from 'src/interfaces';
 
 export const useNotificationList = () => {
@@ -42,41 +41,13 @@ export const useNotificationList = () => {
   return { data, isLoading, error, fetchNextPage, hasNextPage, isFetchingNextPage, listEndRef };
 };
 
-export const useNotificationCard = ({
-  id,
-  message,
-  createdAt,
-  bookmarkId,
-}: Pick<INotification, 'id' | 'message' | 'createdAt' | 'bookmarkId'>) => {
-  const navigate = useNavigate();
+export const useNotificationEdit = ({ id }: Pick<INotification, 'id'>) => {
   const setIsOpen = useSetRecoilState(isMySidebarOpen);
 
-  const handleRoute = () => {
-    if (bookmarkId) {
-      navigate(`/bookmarks/${bookmarkId}`);
-    } else {
-      NOTIFICATION_ROUTES.forEach((notification) => {
-        if (notification.message.substring(0, 2) === message.substring(0, 2)) {
-          navigate(notification.to);
-        }
-      });
-    }
-  };
-
-  const handleClickNotificationCard = async () => {
+  const handleCardClick = async () => {
     await editNotification({ id, isRead: true });
-    handleRoute();
     setIsOpen(false);
   };
 
-  const getPassedDate = () => {
-    const calculatedPassedDate = Math.floor(
-      Math.floor(new Date().getTime() - new Date(createdAt).getTime()) / 1000 / 60 / 60 / 24,
-    );
-    return calculatedPassedDate;
-  };
-
-  const passedDate = useMemo(() => getPassedDate(), [createdAt]);
-
-  return { onClick: handleClickNotificationCard, passedDate };
+  return { onClick: handleCardClick };
 };
