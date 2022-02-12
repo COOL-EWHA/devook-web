a1341a200caf6f08257fac1d9ac716d37e341502
eun-ko <koeun0529@gmail.com>
2022-02-12 11:28:17 +0900

    refactor: :recycle: NotificationCard에 useNotificationEdit ,Link, Img 컴포넌트 적용 및 dayjs의 fromNow적용

--- src/components/my/NotificationCard.tsx
+++ src/components/my/NotificationCard.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import dayjs from 'dayjs';
 
-import { P } from 'src/components/common';
-import { useNotificationCard } from 'src/lib/hooks';
+import { P, Img, Link } from 'src/components/common';
+import { useNotificationEdit } from 'src/lib/hooks';
 import { INotification } from 'src/interfaces';
 import { CACTUS_GREEN, GREY, WHITE } from 'src/constant';
 
-function NotificationCard({ id, message, createdAt, isRead, bookmarkId }: INotification) {
-  const { onClick, passedDate } = useNotificationCard({ id, message, createdAt, bookmarkId });
+function NotificationCard({ id, type, message, createdAt, isRead, bookmarkId }: INotification) {
+  const { onClick } = useNotificationEdit({ id });
+
+  const to = {
+    'no-bookmarks': '/bookmarks',
+    'to-read': '/to-read',
+    'due-date': `/bookmarks/${bookmarkId}`,
+  }[type];
 
   return (
-    <Wrapper isRead={isRead} onClick={onClick}>
-      <LogoImg src="/favicon.svg" alt="devook 로고 이미지" />
+    <StyledLink to={to} isRead={isRead} onClick={onClick}>
+      <StyledImg src="/favicon.svg" alt="devook 로고 이미지" />
       <ContentWrapper>
         <P fontSize="1.4rem">{message}</P>
-        <CreatedAt>{passedDate}일 전</CreatedAt>
+        <CreatedAt>{dayjs(createdAt).fromNow()}</CreatedAt>
       </ContentWrapper>
-    </Wrapper>
+    </StyledLink>
   );
 }
 
 export default NotificationCard;
 
-const Wrapper = styled.div<{ isRead: boolean }>`
+const StyledLink = styled(Link)<{ isRead: boolean }>`
   display: flex;
+  width: fit-content;
   margin: 0 -2rem;
   padding: 2rem;
   background-color: ${({ isRead }) => (isRead ? WHITE : CACTUS_GREEN[100])};
-  cursor: pointer;
 `;
 
-const LogoImg = styled.img`
-  width: 3.2rem;
-  height: fit-content;
+// eslint-disable-next-line react/jsx-props-no-spreading
+const StyledImg = styled((props) => <Img {...props} />).attrs({ width: '3.2rem', height: 'fit-content' })`
   margin-right: 1.2rem;
 `;
 
