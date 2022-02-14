20e54ea311ca9c2641f923f2ace0b055f9aa39f5
eun-ko <koeun0529@gmail.com>
2022-02-14 13:52:11 +0900

    feat: :sparkles: isOnCompleteDisabled 적용 및 form 태그 추가

--- src/components/bookmarks/CreateButton.tsx
+++ src/components/bookmarks/CreateButton.tsx
@@ -9,7 +9,7 @@ const Input = lazy(() => import('src/components/common/Input'));
 const Textarea = lazy(() => import('src/components/common/Textarea'));
 
 export default function BookmarkCreateButton() {
-  const { form, onChange, onSubmit, openModal, closeModal, isModalOpen } = useBookmarkCreate();
+  const { form, onChange, onSubmit, openModal, closeModal, isModalOpen, isOnCompleteDisabled } = useBookmarkCreate();
   const { url, memo } = form;
 
   return (
@@ -17,15 +17,28 @@ export default function BookmarkCreateButton() {
       <IconButton iconType="add" iconWidth="2.8rem" onClick={openModal} />
       <Suspense fallback={null}>
         {isModalOpen && (
-          <StyledModal onClose={closeModal} onComplete={onSubmit} title="북마크 추가하기">
-            <Input name="url" value={url} onChange={onChange} label="링크" placeholder="북마크할 링크를 입력해주세요" />
-            <Textarea
-              name="memo"
-              value={memo}
-              onChange={onChange}
-              label="메모"
-              placeholder="이 북마크와 관련된 메모를 입력해보세요"
-            />
+          <StyledModal
+            onClose={closeModal}
+            onComplete={onSubmit}
+            title="북마크 추가하기"
+            isOnCompleteDisabled={isOnCompleteDisabled}
+          >
+            <form>
+              <Input
+                name="url"
+                value={url}
+                onChange={onChange}
+                label="링크"
+                placeholder="북마크할 링크를 입력해주세요"
+              />
+              <Textarea
+                name="memo"
+                value={memo}
+                onChange={onChange}
+                label="메모"
+                placeholder="이 북마크와 관련된 메모를 입력해보세요"
+              />
+            </form>
           </StyledModal>
         )}
       </Suspense>
