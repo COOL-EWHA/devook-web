c8c59654eea12550cce8359aa3bc69110574be0c
gywlsp <gywls517@gmail.com>
2022-02-17 16:09:34 +0900

    test: :white_check_mark: 포스트 북마크 추가 테스트 코드 리팩토링

--- cypress/integration/2-with-login/postList.spec.ts
+++ cypress/integration/2-with-login/postList.spec.ts
@@ -15,29 +15,19 @@ describe('로그인 추천 글 목록 테스트', () => {
   });
 
   it('포스트를 북마크에 추가하는 기능이 잘 작동하는지 확인한다.', () => {
-    cy.react('PostPreviewCard')
-      .eq(0)
-      .then(($postCard) => {
-        cy.wrap($postCard)
-          .find('button')
-          .contains('북마크 추가')
-          .click()
-          .then(($addButton) => {
-            cy.wrap($addButton).should('have.attr', 'disabled');
-          });
+    cy.react('PostPreviewCard').eq(0).find('button').contains('북마크 추가').click().should('have.attr', 'disabled');
 
+    cy.get('[data-cy=postPreviewCardTitle]')
+      .eq(0)
+      .invoke('text')
+      .then((postTitle) => {
+        cy.visit('/bookmarks');
+        cy.waitForReact();
         cy.get('[data-cy=postPreviewCardTitle]')
           .eq(0)
           .invoke('text')
-          .then((postTitle) => {
-            cy.visit('/bookmarks');
-            cy.waitForReact();
-            cy.get('[data-cy=postPreviewCardTitle]')
-              .eq(0)
-              .invoke('text')
-              .then((bookmarkTitle) => {
-                expect(bookmarkTitle).to.eq(postTitle);
-              });
+          .then((bookmarkTitle) => {
+            expect(bookmarkTitle).to.eq(postTitle);
           });
       });
   });
