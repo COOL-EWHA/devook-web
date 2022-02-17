790936b4ad17827cfbbc71b66642dffa9eb7f4b2
gywlsp <gywls517@gmail.com>
2022-02-17 10:06:59 +0900

    ci: :wrench: cypress env에 API_HOST 환경변수 추가

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -24,6 +24,7 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
 
   // copy needed variables from process.env to config.env
+  config.env.apiHost = process.env.REACT_APP_API_HOST;
   config.env.googleRefreshToken = process.env.GOOGLE_REFRESH_TOKEN;
   config.env.googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
   config.env.googleClientSecret = process.env.REACT_APP_GOOGLE_CLIENT_SECRET;
