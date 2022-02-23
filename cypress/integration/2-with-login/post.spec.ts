aabe20f256835302fb50dfeeebced51a7abcc1a7
gywlsp <gywls517@gmail.com>
2022-02-24 08:19:03 +0900

    test: :white_check_mark: 포스트 북마크 추가 테스트 코드 mouseover 후 북마크 리스트 fetch 확인하게 변경

--- cypress/integration/2-with-login/post.spec.ts
+++ cypress/integration/2-with-login/post.spec.ts
@@ -14,11 +14,11 @@ describe('로그인 추천 글 목록 테스트', () => {
   it('포스트를 북마크에 추가하는 기능이 잘 작동하는지 확인한다.', () => {
     cy.react('PostPreviewCard').eq(0).find('button').contains('북마크 추가').click();
     cy.getReact('PostPreviewCard').nthNode(0).getProps('title').as('postTitle');
-
-    cy.get('li').contains('bookmarks').trigger('mouseenter').click();
     const apiHost = Cypress.env('apiHost');
     cy.intercept('GET', `${apiHost}/bookmarks`).as('getBookmarks');
+    cy.get('li').contains('bookmarks').trigger('mouseover');
     cy.wait('@getBookmarks');
+    cy.get('li').contains('bookmarks').click();
     cy.get('@postTitle').then((postTitle) => {
       cy.getReact('PostPreviewCard').nthNode(0).getProps('title').should('eq', postTitle);
     });
