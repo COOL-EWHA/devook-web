5158cc4c9c315568e0b9b606533d895d67a5d0f6
gywlsp <gywls517@gmail.com>
2022-02-23 19:47:08 +0900

    test: :white_check_mark: 포스트 북마크 추가 기능 테스트 코드 리팩토링

--- cypress/integration/2-with-login/post.spec.ts
+++ cypress/integration/2-with-login/post.spec.ts
@@ -13,18 +13,11 @@ describe('로그인 추천 글 목록 테스트', () => {
 
   it('포스트를 북마크에 추가하는 기능이 잘 작동하는지 확인한다.', () => {
     cy.react('PostPreviewCard').eq(0).find('button').contains('북마크 추가').click();
+    cy.getReact('PostPreviewCard').nthNode(0).getProps('title').as('postTitle');
 
-    cy.get('[data-cy=postPreviewCardTitle]')
-      .eq(0)
-      .invoke('text')
-      .then((postTitle) => {
-        cy.get('li').contains('bookmarks').click();
-        cy.get('[data-cy=postPreviewCardTitle]')
-          .eq(0)
-          .invoke('text')
-          .then((bookmarkTitle) => {
-            expect(bookmarkTitle).to.eq(postTitle);
-          });
-      });
+    cy.get('li').contains('bookmarks').click();
+    cy.get('@postTitle').then((postTitle) => {
+      cy.getReact('PostPreviewCard').nthNode(0).getProps('title').should('eq', postTitle);
+    });
   });
 });
