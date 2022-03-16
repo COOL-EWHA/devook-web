5b20cd142da3d51157591f6b8745235b2dedda37
gywlsp <gywls517@gmail.com>
2022-03-16 22:45:07 +0900

    chore: :label: window interface에 AuthChannel property 추가

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,9 @@ export * from './INotification';
 export * from './IUser';
 export * from './IPost';
 export * from './IBookmark';
+
+declare global {
+  interface Window {
+    AuthChannel: Window;
+  }
+}
