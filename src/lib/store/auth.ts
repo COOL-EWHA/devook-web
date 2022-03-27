a665974703122fa2e3d4689bf36d9614647e75fd
gywlsp <gywls517@gmail.com>
2022-03-27 12:41:22 +0900

    feat: :sparkles: recoil atom accessToken 추가

--- src/lib/store/auth.ts
+++ src/lib/store/auth.ts
@@ -1,4 +1,5 @@
 import { atom } from 'recoil';
 
 export const isAuthLoading = atom<boolean>({ key: 'isAuthLoading', default: true });
+export const accessToken = atom<string>({ key: 'accessToken', default: '' });
 export const isUserLoggedIn = atom<boolean>({ key: 'isUserLoggedIn', default: false });
