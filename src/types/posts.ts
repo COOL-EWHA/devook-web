3bcd883042c88393b0fc4279752522d92dbbf999
eun-ko <koeun0529@gmail.com>
2022-05-21 23:19:14 +0900

    feat: :label: PostPreview 타입에 value 속성 추가

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -2,7 +2,7 @@ import { IPost } from 'src/interfaces';
 import { ListRequestParams } from '.';
 
 export type PostType = 'bookmark' | 'post';
-export type PostPreview = IPost & { isBookmarked?: boolean };
+export type PostPreview = IPost & { isBookmarked?: boolean; value?: number };
 export type PostListParams = ListRequestParams & Pick<IPost, 'tags'> & { q?: string };
 export type RelatedPostListParams = BookmarkRelatedPostListParams | PostRelatedPostListParams;
 export type BookmarkRelatedPostListParams = ListRequestParams & { bookmarkId: number };
