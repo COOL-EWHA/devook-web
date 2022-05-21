9a6b2f4f68f6f6425cfaa3280e6b2b22a2cfe980
eun-ko <koeun0529@gmail.com>
2022-05-21 23:09:43 +0900

    refactor: :label: IPost 안에있던 isBookmarked 속성을 PostPreview 타입쪽으로 분리
    불필요하게 전체 속성들을 Pick으로 뽑아쓰고 있던 로직 수정
    
    IBookmark가 IPost를 상속해서 쓰고 있으므로, isBookmarked 속성은 IPost에 있기에 부적절

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -2,7 +2,7 @@ import { IPost } from 'src/interfaces';
 import { ListRequestParams } from '.';
 
 export type PostType = 'bookmark' | 'post';
-export type PostPreview = Pick<IPost, 'id' | 'title' | 'thumbnail' | 'description' | 'url' | 'tags' | 'isBookmarked'>;
+export type PostPreview = IPost & { isBookmarked?: boolean };
 export type PostListParams = ListRequestParams & Pick<IPost, 'tags'> & { q?: string };
 export type RelatedPostListParams = BookmarkRelatedPostListParams | PostRelatedPostListParams;
 export type BookmarkRelatedPostListParams = ListRequestParams & { bookmarkId: number };
